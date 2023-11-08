adc8564e470309c5bba3fbca104c7df912aa9394
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-11-08 07:05:04 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,7 +30,15 @@ def stats() -> str:
 @app_views.route("/unauthorized", strict_slashes=False)
 def unauthorized() -> str:
     """GET /api/v1/unauthorized
-    Return:
-      - Aborts
+    Aborts(401)
     """
     abort(401)
+
+
+@app_views.route("/forbidden", strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/unauthorized
+    Aborts(403)
+    """
+    abort(403)
