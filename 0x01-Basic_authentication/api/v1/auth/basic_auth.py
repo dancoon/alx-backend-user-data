a0e98bc56bab13a920de396ba50ccfc69f94f1a4
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-11-09 21:25:51 +0300

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,13 +2,14 @@
 """ Basic Auth"""
 from api.v1.auth.auth import Auth
 import base64
+from re import search
 
 
 class BasicAuth(Auth):
     """inherits from Auth"""
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """returns the Base64 part of the Authorization header"""
         if authorization_header is None:
             return None
@@ -20,8 +21,9 @@ class BasicAuth(Auth):
         return header
 
     def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
-        """ returns the decoded value of a Base64 string """
+        self, base64_authorization_header: str
+    ) -> str:
+        """returns the decoded value of a Base64 string"""
         if not base64_authorization_header:
             return None
         if not isinstance(base64_authorization_header, str):
@@ -31,3 +33,14 @@ class BasicAuth(Auth):
         except Exception as e:
             return None
         return header_bytes.decode("utf-8")
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """returns the user email and password from the Base64 decoded value"""
+        decoded = decoded_base64_authorization_header
+        if (decoded and isinstance(decoded, str) and
+                ":" in decoded):
+            res = decoded.split(":", 1)
+            return (res[0], res[1])
+        return (None, None)
