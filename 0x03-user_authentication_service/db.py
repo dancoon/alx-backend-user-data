ec55c9bf873b138fd0c7a3f86bb2b2c87b7c6da2
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-11-14 21:56:22 +0300

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,12 +33,13 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """save the user to the database and return the User"""
-        if email is None or hashed_password is None:
-            return
-        user = User(email=email, hashed_password=hashed_password)
-        session = self._session
-        session.add(user)
-        session.commit()
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            user = None
         return user
 
     def find_user_by(self, **kwargs) -> User:
