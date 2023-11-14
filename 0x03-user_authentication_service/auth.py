6b6c7273f93e364b995d832a0fe46a62b27c6c2b
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-11-14 08:12:18 +0300

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    _hash_password
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
