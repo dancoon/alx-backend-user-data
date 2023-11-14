19fd86664ffb841434829dbb0e9a0d9352a2bcdd
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-11-14 08:21:24 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,10 @@
 Auth module
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +14,21 @@ def _hash_password(password: str) -> bytes:
     _hash_password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        """Initialize a new auth instance"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
